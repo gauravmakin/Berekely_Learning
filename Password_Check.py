c02b6a996a45b7079843701b12540c3bec5a9f55
Gaurav <makingaurav21@gmail.com>
2018-05-27 16:43:21 -0700

    PasswordChecker_First
    Firstcommit

---
+++ Password_Check.py
@@ -0,0 +1,16 @@
+correct_password = "python123"
+
+name = input("Enter Name: ")
+password =  input("Enter Password: ")
+
+while password != correct_password:
+    print("Wrong Password. Enter Again")
+    password = input("Enter Password: ")
+
+print("Hi %s You are Logged In" % name)
+
+a = ['a','b','c']
+b = [1,2,3]
+
+for i, j in zip(a,b):
+    print("%s is %s" %(i,j))
