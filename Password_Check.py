60b5b1a129a11ca7d922861d746a215eb7926b1e
Gaurav <gauravmakin@users.noreply.github.com>
2018-05-29 11:21:30 -0700

    Testing Clean

--- Password_Check.py
+++ Password_Check.py
@@ -14,6 +14,3 @@ b = [1,2,3]
 
 for i, j in zip(a,b):
     print("%s is %s" %(i,j))
-
-print("This is just a test of github repository")
-print("This is the second test from the PC. Is this different. Still syncing?")
