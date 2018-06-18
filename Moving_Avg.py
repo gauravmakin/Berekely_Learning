f911842741583894904d5fa4f8c0fb62d8902271
Gaurav <gauravmakin@users.noreply.github.com>
2018-06-18 14:37:12 -0700

    Commit

--- Moving_Avg.py
+++ Moving_Avg.py
@@ -4,6 +4,7 @@ import pylab as plb
 import matplotlib.pyplot as plt
 
 a = np.random.randint(1,10,100)
+a = [1, 2, 3, 4, 5, 6, 7]
 print(a)
 
 def moving_avg(dataset, window):
@@ -20,11 +21,26 @@ def exp_moving_avg(dataset, window):
     return expon
 
 b = moving_avg(a, 3)
+print(b)
 c = exp_moving_avg(a, 3)
 
-fig = plb.figure()
-fig, axes = plb.subplots(nrows=2, ncols=1)
-axes[0].plot(a,c,color='red', lw=3, ls='--', label='Scribble')
-axes[0].set_title('Moving Average vs Exponential Average')
-axes[1].plot(a,plb.sin(a))
-plb.show()
+#fig = plb.figure()
+# fig, axes = plb.subplots(nrows=2, ncols=1)
+# axes[0].plot(a,c,color='red', lw=3, ls='--', label='Scribble')
+# axes[0].set_title('Moving Average vs Exponential Average')
+# axes[1].plot(a,plb.sin(a))
+# plb.pause(2)
+
+
+# Does not match with what we are getting otherwise
+cumsum, moving_avs = [0], []
+for i, x in enumerate(a, 1):
+    cumsum.append(cumsum[i-1] + x)
+    if i >= 3:
+        moving_ave = (cumsum[i] - cumsum[i-1])/3
+        moving_avs.append(moving_ave)
+print(moving_avs)
+
+
+print("\n_____________________\n")
+print(np.convolve(a, np.ones((3,))/3, mode = 'valid'))
